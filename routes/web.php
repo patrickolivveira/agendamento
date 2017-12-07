4feed08c9488c3625b02f2cbdd3b66286c1d415c
Patrick Oliveira <oliveira.patrick94@gmail.com>
2017-12-06 23:52:13 -0200

    ajuste de rotas

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,15 @@
 Auth::routes();
 
 Route::get('/', 'IndexController@index');
-Route::get('entrar', 'IndexController@login');
-Route::get('cadastrar', 'IndexController@register');
+Route::get('/entrar', 'IndexController@login');
+Route::get('/cadastrar', 'IndexController@register');
 
-Route::get('home', 'HomeController@index')->name('home');
+// Route::get('home', 'HomeController@index')->name('home');
 
-Route::resource('pacientes', 'PacientesController');
+Route::get('/home', 'HomeController@index');
 
-Route::resource('medicos', 'MedicosController');
+Route::resource('/pacientes', 'PacientesController');
 
-Route::resource('agendamentos', 'AgendamentosController');
+Route::resource('/medicos', 'MedicosController');
+
+Route::resource('/agendamentos', 'AgendamentosController');
